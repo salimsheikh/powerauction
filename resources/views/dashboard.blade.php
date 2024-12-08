d83918867257a1713988553e3ddd62d243019d9f
salimsheikh <salimsheikh4u2000@gmail.com>
2024-12-08 22:03:26 +0530

    Dashboard designing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,93 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="admin-page">
                 <div class="p-6 text-gray-900 dark:text-white">
-                    {{ __("You're logged in!") }}
+                    
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+                    <div class="mt-4">
+                        <div class="flex flex-wrap -mx-6">
+                            <div class="w-full px-6 sm:w-1/2 xl:w-1/2">
+                                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+                                    <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
+                                        <span class="material-icons">text_fields</span>
+                                    </div>
+                
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">Levels premier league 2023 Mens</h4>
+                                        <span class="material-icons">group_add</span>
+                                    </div>
+                                </div>
+                            </div>
+                
+                            <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/2 sm:mt-0">
+                                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+                                    <div class="p-3 rounded-full bg-orange-600 bg-opacity-75">
+                                        <span class="material-icons">people</span>
+                                    </div>
+                
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">200,521</h4>
+                                        <div class="text-gray-500">Total No of Teams</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+
+                    <div class="mt-4">
+                        <div class="flex flex-wrap -mx-6">
+
+                            <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+                                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+                                    <div class="p-3 rounded-full bg-pink-600 bg-opacity-75">
+                                        <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"/>
+                                            <path d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z" stroke="currentColor" stroke-width="2"/>
+                                        </svg>
+                                    </div>
+                
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">215,542</h4>
+                                        <div class="text-gray-500">Available Products</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+                                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+                                    <div class="p-3 rounded-full bg-pink-600 bg-opacity-75">
+                                        <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"/>
+                                            <path d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z" stroke="currentColor" stroke-width="2"/>
+                                        </svg>
+                                    </div>
+                
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">215,542</h4>
+                                        <div class="text-gray-500">Available Products</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+                                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+                                    <div class="p-3 rounded-full bg-pink-600 bg-opacity-75">
+                                        <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"/>
+                                            <path d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z" stroke="currentColor" stroke-width="2"/>
+                                        </svg>
+                                    </div>
+                
+                                    <div class="mx-5">
+                                        <h4 class="text-2xl font-semibold text-gray-700">215,542</h4>
+                                        <div class="text-gray-500">Available Products</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
